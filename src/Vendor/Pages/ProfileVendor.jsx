c3e07317075c3b0c9fe5d01cedda94be4913c77d
praveenaa47 <pravipraveena722@gmail.com>
2025-04-26 15:24:37 +0530

    edit commit

--- src/Vendor/Pages/ProfileVendor.jsx
+++ src/Vendor/Pages/ProfileVendor.jsx
@@ -58,8 +58,8 @@ function ProfileVendor() {
             
             {/* Post code field - highlighted with border */}
             <div className="relative">
-              <div className="bg-white border-2 border-blue-400 rounded-md p-3 pr-10 relative">
-                <span className="text-gray-700">Enter your post code</span>
+              <div className="bg-blue-100 rounded-md p-3 pr-10 relative">
+                <span className="text-gray-700">Enter your postcode</span>
                 <button className="absolute right-3 top-3 text-gray-500">
                   <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
