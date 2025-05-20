8013cf9e55dbae12944b9c727881838455cbb084
Kajahussain123 <kajahusain513@gmail.com>
2025-05-20 14:45:09 +0530

    updates

--- src/Components/Home/Section1.jsx
+++ src/Components/Home/Section1.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import HomeImg from '../../assets/Home.png'
 
 const Banner = () => {
   const navigate = useNavigate();
@@ -11,7 +12,7 @@ const Banner = () => {
 
   return (
     <div className="relative w-full h-[650px] mb-12 rounded-xl overflow-hidden" data-aos="fade-up">
-      <img src="/src/assets/Home.png" alt="Banner" className="w-full h-full object-cover" />
+      <img src={HomeImg} alt="Banner" className="w-full h-full object-cover" />
       <div className="absolute inset-0 bg-opacity-40 flex items-center px-8 md:px-16">
         <div className="text-[#3A7EDD] max-w-md mt-52" data-aos="fade-right" data-aos-delay="200">
           <h2 className="text-3xl md:text-4xl font-bold mt-5" data-aos="fade-down" data-aos-delay="400">
